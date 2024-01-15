aee40042b9a1ab70e60d59e1a0ae00947b347cc8
pandawaa <pallakangoding@gmail.com>
2024-01-15 15:20:19 +0800

    Finish Project

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
-        <Image src="/hilink-logo.svg" alt="logo" width={74} height={24} />
+        <Image src="/test.png" alt="logo" width={160} height={40} />
       </Link>
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
@@ -27,6 +27,7 @@ const Navbar = () => {
           title="Login"
           icon="/user.svg"
           variant="btn_dark_green"
+          full
         />
       </div>
 
